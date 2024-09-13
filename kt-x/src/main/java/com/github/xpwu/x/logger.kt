07930eb7b4f5e43b1a8262646edde32036f249ae
xpwu <diodexp@foxmail.com>
2024-09-14 02:21:55 +0800

    add println logger

--- kt-x/src/main/java/com/github/xpwu/x/logger.kt
+++ kt-x/src/main/java/com/github/xpwu/x/logger.kt
@@ -1,6 +1,9 @@
 package com.github.xpwu.x
 
+import android.os.Build
 import android.util.Log
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
 
 interface Logger {
 	fun Debug(tag: String, msg: String)
@@ -26,4 +29,29 @@ class AndroidLogger: Logger {
 		Log.e(tag, msg)
 	}
 
+}
+
+fun FileInfo(): String {
+	val st = Throwable().stackTrace[2]
+	val first = if (st.fileName.length >= 15) st.fileName.length-15 else 0
+	return "${st.fileName.slice(first until st.fileName.length)}:${st.lineNumber}"
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+class PrintlnLogger: Logger {
+	override fun Debug(tag: String, msg: String) {
+		println("${LocalDateTime.now()} ${FileInfo()} Debug: $tag  --->  $msg")
+	}
+
+	override fun Error(tag: String, msg: String) {
+		println("${LocalDateTime.now()} ${FileInfo()} Error: $tag  --->  $msg")
+	}
+
+	override fun Info(tag: String, msg: String) {
+		println("${LocalDateTime.now()} ${FileInfo()} Info: $tag  --->  $msg")
+	}
+
+	override fun Warning(tag: String, msg: String) {
+		println("${LocalDateTime.now()} ${FileInfo()} Warning: $tag  --->  $msg")
+	}
 }
